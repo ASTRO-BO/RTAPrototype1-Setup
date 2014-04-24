3722880ba30c33fafb0e19a3c68ecaad502f5bf1
Andrea Zoli <zoli@iasfbo.inaf.it>
2014-04-24 11:24:28 +0200

    Release v1.0.0.

--- install.sh
+++ install.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bash
 
+trap exit ERR
+
+declare -a cmake_repoinstall=("libQLBase")
 declare -a repo_install=("PacketLib" "RTAtelem" "RTAConfig" "RTAtelemDemo")
 declare -a repo=("RTAEBSimIce" "RTACoreIce" "RTAMonitor")
 
-trap exit err
+for i in "${cmake_repoinstall[@]}"
+do
+    pushd $PWD
+    cd $i
+    if [[ $1 == "clean" ]]
+    then
+        echo
+        echo "Cleaning $i..."
+        rm -rf build
+    else
+        echo
+        echo "Building $i..."
+        mkdir -p build/$i
+        cd build/
+        cmake -DCMAKE_BUILD_TYPE=Debug \
+          -DCMAKE_INSTALL_PREFIX:PATH=$CTARTA \
+          -DBoost_NO_BOOST_CMAKE=ON \
+          ..
+        make VERBOSE=1 -j5
+        echo "Installing $1..."
+        make install
+    fi
+	popd
+done
 
 for i in "${repo_install[@]}"
 do
